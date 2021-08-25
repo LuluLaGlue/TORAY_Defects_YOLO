34aed362cbf9795abdeda03cea847a79bb7255ea
LuluLaGlue <lucien.sigayret@protonmail.com>
2021-08-25 13:55:44 +0200

    misc

--- README.md
+++ README.md
@@ -16,6 +16,6 @@ To create a new model run <code>python ./algo/train.py --img img_size --batch ba
 
 # Detection
 
-To use the previsouly trained model run <code>python detect.py --weights path/to/best/weights --img img_size --conf confidence_threshold --source img_source --save-crop --exist-ok</code> <br />
+To use the previsouly trained model run <code>python detect.py --weights path/to/best/weights --img img_size --conf confidence_threshold --source img_source --save-crop --exist-ok --project folder_to_save</code> <br />
 
 The weights created in training mode can be found in <code>./algo/runs/train/exp/weights</code>. The result are located in <code>./algo/runs/detect/expRunNbr/</code>
--- utilities/split_video.py
+++ utilities/split_video.py
@@ -1,15 +1,16 @@
 import cv2
 import os
 
-for file in os.listdir('.'):
-    if (file.split('.')[1] == "avi"):
-        vidcap = cv2.VideoCapture(file)
+for file in os.listdir('./Video'):
+    if (file.split('.')[len(file.split('.')) - 1] == "avi"):
+        vidcap = cv2.VideoCapture('./Video/' + file)
         success, image = vidcap.read()
         count = 0
         while success:
             # save frame as JPEG file
             cv2.imwrite("./img/{}_frame{}.jpg".format(
                 file.split('.')[0], count), image)
+            # "video", count), image)
             success, image = vidcap.read()
-            print('Read a new frame: ', success)
+            print('Read a new frame: {} From file: {}'.format(count, file))
             count += 1
